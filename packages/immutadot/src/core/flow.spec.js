ff7126b6d4e115d634d2a3e81a02c0148cc7c676
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-02-22 09:35:13 +0100

    :recycle: Refactor flow test and add paths in callback parameter

--- packages/immutadot/src/core/flow.spec.js
+++ packages/immutadot/src/core/flow.spec.js
@@ -21,82 +21,98 @@ describe('flow.flow', () => {
   }
 
   it('should apply modifications', () => {
-    immutaTest((input, path1, path2, path3) => {
-      const output = flow(
-        set(path1, 'value5'),
-        update(path2, value => value.toUpperCase()),
-        unset(path3),
-      )(input)
-      expect(output).toEqual({
-        nested1: {
-          prop1: 'value1',
-          prop2: 'value5',
-        },
-        nested2: { prop3: 'VALUE3' },
-        other: {},
-      })
-      return output
-    }, object, 'nested1.prop2', 'nested2.prop3', 'nested2.prop4')
-
-    immutaTest((input, path1, path2, path3) => {
-      const output = flow([
-        set(path1, 'value5'),
-        update(path2, value => value.toUpperCase()),
-        unset(path3),
-      ])(input)
-      expect(output).toEqual({
-        nested1: {
-          prop1: 'value1',
-          prop2: 'value5',
-        },
-        nested2: { prop3: 'VALUE3' },
-        other: {},
-      })
-      return output
-    }, object, 'nested1.prop2', 'nested2.prop3', 'nested2.prop4')
-
-    immutaTest((input, path1, path2, path3) => {
-      const output = flow(
-        set(path1, 'value5'),
-        [
+    immutaTest(
+      object,
+      ['nested1.prop2', 'nested2.prop3', 'nested2.prop4'],
+      (input, [path1, path2, path3]) => {
+        const output = flow(
+          set(path1, 'value5'),
           update(path2, value => value.toUpperCase()),
           unset(path3),
-        ],
-      )(input)
-      expect(output).toEqual({
-        nested1: {
-          prop1: 'value1',
-          prop2: 'value5',
-        },
-        nested2: { prop3: 'VALUE3' },
-        other: {},
-      })
-      return output
-    }, object, 'nested1.prop2', 'nested2.prop3', 'nested2.prop4')
+        )(input)
+        expect(output).toEqual({
+          nested1: {
+            prop1: 'value1',
+            prop2: 'value5',
+          },
+          nested2: { prop3: 'VALUE3' },
+          other: {},
+        })
+        return output
+      },
+    )
+
+    immutaTest(
+      object,
+      ['nested1.prop2', 'nested2.prop3', 'nested2.prop4'],
+      (input, [path1, path2, path3]) => {
+        const output = flow([
+          set(path1, 'value5'),
+          update(path2, value => value.toUpperCase()),
+          unset(path3),
+        ])(input)
+        expect(output).toEqual({
+          nested1: {
+            prop1: 'value1',
+            prop2: 'value5',
+          },
+          nested2: { prop3: 'VALUE3' },
+          other: {},
+        })
+        return output
+      },
+    )
+
+    immutaTest(
+      object,
+      ['nested1.prop2', 'nested2.prop3', 'nested2.prop4'],
+      (input, [path1, path2, path3]) => {
+        const output = flow(
+          set(path1, 'value5'),
+          [
+            update(path2, value => value.toUpperCase()),
+            unset(path3),
+          ],
+        )(input)
+        expect(output).toEqual({
+          nested1: {
+            prop1: 'value1',
+            prop2: 'value5',
+          },
+          nested2: { prop3: 'VALUE3' },
+          other: {},
+        })
+        return output
+      },
+    )
   })
 
   it('should skip non functions', () => {
-    immutaTest((input, path) => {
-      const output = flow(
-        null,
-        set(path, 'bar'),
-        undefined,
-        false,
-        [
+    immutaTest(
+      {
+        nested: { prop: 'foo' },
+        other: {},
+      },
+      ['nested.prop'],
+      (input, [path]) => {
+        const output = flow(
           null,
+          set(path, 'bar'),
           undefined,
           false,
-        ],
-      )(input)
-      expect(output).toEqual({
-        nested: { prop: 'bar' },
-        other: {},
-      })
-      return output
-    }, {
-      nested: { prop: 'foo' },
-      other: {},
-    }, 'nested.prop')
+          [
+            null,
+            undefined,
+            false,
+          ],
+        )(input)
+        expect(output).toEqual({
+          nested: { prop: 'bar' },
+          other: {},
+        })
+        return output
+      },
+    )
   })
 
   it('should do nothing if empty flow', () => {
